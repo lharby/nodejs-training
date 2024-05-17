67a51f9465db78b72f7b08257c365fb92c08dc86
Luke Harby <Luke.Harby@akqa.com>
2024-05-17 09:38:36 +0100

    New

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>');
+    res.write("</html>");
+    return res.end();
+  }
+  if (req.url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Dummy text");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Response title</title></head>");
+  res.write("<body><p>This is the response, well done</p></body>");
+  res.write("</html>");
+  res.end();
+  // process.exit();
+});
+
+server.listen(3000);
