e8d1c61c85da0153b60a9fac5126cd5b3c0be5f6
Luke Harby <lukeharby@Lukes-MacBook-Pro.local>
2024-05-21 09:08:00 +0100

    import export

--- server.js
+++ server.js
@@ -1,38 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter message</title></head>");
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>');
-    res.write("</html>");
-    return res.end();
-  }
-  if (req.url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
-      res.statusCode = 302;
-      res.setHeader("Location", "/");
-      return res.end();
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>Response title</title></head>");
-  res.write("<body><p>This is the response, well done</p></body>");
-  res.write("</html>");
-  res.end();
-  // process.exit();
-});
+const routes = require('./routes.js');
+
+const server = http.createServer(routes);
 
 server.listen(3000);
